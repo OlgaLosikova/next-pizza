84c3060fe51e8cc65d475e4e9b1762d248006530
losikova.o <losikova.o@modimio.ru>
2025-12-19 14:42:08 +0300

    first commit

---
+++ app/product/[id]/page.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@radix-ui/react-checkbox";
+
+export default function test (){
+    return <> <Button variant={'outline'}>dfjkksdhfkjh</Button>123
+    <Checkbox>xtr1</Checkbox></>
+}
